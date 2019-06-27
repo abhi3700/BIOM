d1438805a40aa48c1e6adcdccef74c0c2d2ab758
Abhijit Roy <abhijit@scl.gov.in>
2019-06-27 17:29:56 +0530

    biom modified to final ouput

--- biom/biom.py
+++ biom/biom.py
@@ -7,6 +7,8 @@ sht_data = 'IntnlCmpsReport'
 sht_main = 'Main'
 columns_required = ['Emp Code', 'Emp Name', 'Reader', 'Date', 'Time', 'Status']
 
+
+
 def main():
     wb = xw.Book.caller()
     # wb.sheets[0].range("A1").value = "Hello xlwings!"
@@ -22,8 +24,7 @@ def main():
     df = df.drop(['Unnamed: 0', 'Sr.No.'], axis=1)      # drop columns with header names - ['Unnamed: 0', 'Sr.No.']
     df = df[(df['Reader'] != 'Reader') & (df['Reader'] != 'NaN')]       # delete rows with values - 'Reader' & 'NIL'
     df.fillna(method= 'ffill', inplace=True)    # fill 'NaN' with previous values using 'ffill'
-    # df = df[(df['Reader'] == "M FAB - 1") & (df['Reader'] == "M FAB")]
-    # df = df[(df['Emp Name'] != "NITESH KUMAR SINHA ") & (df['Emp Name'] != "ABHIJIT ROY ")]
+    df = df[(df['Reader'] == 'M FAB - 1') | (df['Reader'] == 'M FAB')]      # extract dataframe with column header names - 'M FAB - 1' or 'M FAB'
     # print(df)
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------    
     # display output to Excel sheet - 'Main'
